8755f14ef5ac256e0132f417fcc442c1e73cefbe
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-01-15 11:34:48 +0700

    Initial commit

---
+++ SK-Lib-stdio.h-FILE_C/Source.c
@@ -0,0 +1,24 @@
+#pragma warning(disable:4996)
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    FILE* pFile;
+    char buffer[100];
+
+    pFile = fopen("myfile.txt", "r");
+    if (pFile == NULL) {
+        perror("Error opening file");
+    } else {
+        while (!feof(pFile)) {
+            if (fgets(buffer, 100, pFile) == NULL) break;
+            fputs(buffer, stdout);
+        }
+
+        fclose(pFile);
+    }
+
+    _getch();
+    return 0;
+}
